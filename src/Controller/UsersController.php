9d151cf954b87e42796f205a543f193f6f1a11a8
baestero <leonardobaestero@gmail.com>
2026-06-23 23:35:40 -0300

    debug

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -46,23 +46,19 @@ class UsersController extends AppController
   public function add()
   {
     $this->viewBuilder()->setLayout('login');
-
     $user = $this->Users->newEmptyEntity();
 
-
     if ($this->request->is('post')) {
-
-      $user = $this->Users->patchEntity($user, $this->request->getData());
-
-      if ($this->Users->save($user)) {
-
-        $this->Flash->success(__('Usuário cadastrado com sucesso.'));
-
-        return $this->redirect(['action' => 'login']);
+      try {
+        $user = $this->Users->patchEntity($user, $this->request->getData());
+        if ($this->Users->save($user)) {
+          $this->Flash->success(__('Usuário cadastrado com sucesso.'));
+          return $this->redirect(['action' => 'login']);
+        }
+        $this->Flash->error(json_encode($user->getErrors()));
+      } catch (\Exception $e) {
+        $this->Flash->error($e->getMessage() . ' | ' . $e->getFile() . ':' . $e->getLine());
       }
-
-
-      $this->Flash->error(__('Não foi possível cadastrar usuário, verifique os erros abaixo.'));
     }
 
     $this->set(compact('user'));
